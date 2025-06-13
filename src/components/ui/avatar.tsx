fcc8b121193df25a0c338224536689fda95c8f0d
Ritmo Hung <jajh102098@gmail.com>
2025-06-13 12:05:34 +0800

    Chores: Migrate to radix-ui mono package

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import * as AvatarPrimitive from "@radix-ui/react-avatar"
+import { Avatar as AvatarPrimitive } from "radix-ui"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
